ad400c362af8fb116716cb0bcf613991f54f6596
Hyeseong Kim <hey@hyeseong.kim>
2021-08-25 03:47:46 +0900

    fix: install command

--- utils.sh
+++ utils.sh
@@ -3,16 +3,11 @@
 set -eo pipefail
 
 GITHUB_REPO="https://github.com/grain-lang/grain"
-REGISTRY_URL="https://api.github.com/repos/grain-lang/grain/releases"
 
 cmd="curl -s"
 if [ -n "$GITHUB_API_TOKEN" ]; then
  cmd="$cmd -H 'Authorization: token $GITHUB_API_TOKEN'"
 fi
-
-function download_meatadata() {
-  $cmd "$REGISTRY_URL"
-}
 
 # stolen from https://github.com/rbenv/ruby-build/pull/631/files#diff-fdcfb8a18714b33b07529b7d02b54f1dR942
 function sort_versions() {
@@ -20,8 +15,10 @@ function sort_versions() {
     LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
 }
 
-function extract_version() {
-  grep -oE "tag_name\": \"grain-v.{1,20}\"," | sed 's/tag_name\": \"grain-v//;s/\",//'
+function list_github_tags() {
+  git ls-remote --tags --refs "$GITHUB_REPO" |
+    grep -o 'refs/tags/grain-v.*' | cut -d/ -f3- |
+    sed 's/^grain-v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
 }
 
 function get_platform() {
